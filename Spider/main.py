71558e4d64b9a585f31b21cb86c6616e74c1d9c0
shubh2565 <shubh2565@users.noreply.github.com>
2019-03-12 11:59:00 +0530

    Add files via upload

---
+++ Spider/main.py
@@ -0,0 +1,49 @@
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from web1 import *
+
+PROJECT_NAME = 'wikipedia'
+HOMEPAGE = 'https://www.wikipedia.org/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 4
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+# Create worker threads
+def create_workers():
+	for _ in range(NUMBER_OF_THREADS):
+		t = threading.Thread(target = work())
+		t.daemon = True
+		t.start()
+
+
+# Do the next job in the queue
+def work():
+	while True:
+		url = queue.get()
+		Spider.crawl_page(threading.current_thread().name, url)
+		queue.task_done()
+
+
+# Each queued link is a new job
+def create_jobs():
+	for link in file_to_set(QUEUE_FILE):
+		queue.put(link)
+	queue.join()
+	crawl()
+
+# Check for items to crawl
+def crawl():
+	queued_links = file_to_set(QUEUE_FILE)
+	if len(queued_links) > 0:
+		print(str(len(queued_links)) + ' links in the queue')
+		create_jobs()
+
+
+create_workers()
+crawl()
